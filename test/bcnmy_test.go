66670d0686e8c31af3181d07d8156ad17866e4cc
oblzh <openblocklzh@gmail.com>
2023-02-16 19:00:54 +0800

    uncomment and make fmt

--- test/bcnmy_test.go
+++ test/bcnmy_test.go
@@ -19,91 +19,91 @@ func buildBcnmy() *metax.Bcnmy {
 	return b
 }
 
-//// Finished
-//func TestDeleteContract(t *testing.T) {
-//b := buildBcnmy()
+// Finished
+func TestDeleteContract(t *testing.T) {
+	b := buildBcnmy()
 
-//data := &metax.DeleteContractRequest{
-//ContractAddress: "0x0a364431476a8d1dd475590b0a028b40686ce542",
-//ContractType:    "SC",
-//}
+	data := &metax.DeleteContractRequest{
+		ContractAddress: "0x0a364431476a8d1dd475590b0a028b40686ce542",
+		ContractType:    "SC",
+	}
 
-//resp, err := b.DeleteContract(data)
-//assert.Nil(t, err)
-//assert.NotNil(t, resp)
-//assert.Equal(t, resp.Code, 143)
-//}
+	resp, err := b.DeleteContract(data)
+	assert.Nil(t, err)
+	assert.NotNil(t, resp)
+	assert.Equal(t, resp.Code, 143)
+}
 
-//// Finished
-//func TestDeleteMethod(t *testing.T) {
-//b := buildBcnmy()
+// Finished
+func TestDeleteMethod(t *testing.T) {
+	b := buildBcnmy()
 
-//data := &metax.DeleteMethodRequest{
-//ContractAddress: "0xa6b71e26c5e0845f74c812102ca7114b6a896ab2",
-//Method:          "createProxyWithNonce",
-//}
+	data := &metax.DeleteMethodRequest{
+		ContractAddress: "0xa6b71e26c5e0845f74c812102ca7114b6a896ab2",
+		Method:          "createProxyWithNonce",
+	}
 
-//resp, err := b.DeleteMethod(data)
-//assert.Nil(t, err)
-//assert.NotNil(t, resp)
-//assert.Equal(t, resp.Code, 143)
-//}
+	resp, err := b.DeleteMethod(data)
+	assert.Nil(t, err)
+	assert.NotNil(t, resp)
+	assert.Equal(t, resp.Code, 143)
+}
 
-//// Finished
-//func TestAddContract(t *testing.T) {
-//b := buildBcnmy()
-//assert.NotNil(t, b)
+// Finished
+func TestAddContract(t *testing.T) {
+	b := buildBcnmy()
+	assert.NotNil(t, b)
 
-//data := &metax.AddContractRequest{
-//ContractName:        "TestToken",
-//ContractAddress:     "0xeaC94633FFf8C65aD9EFdCF237741D931fa995Cd",
-//ContractType:        "SC",
-//WalletType:          "",
-//MetaTransactionType: "DEFAULT",
-//ABI:                 token.TestTokenABI,
-//}
+	data := &metax.AddContractRequest{
+		ContractName:        "TestToken",
+		ContractAddress:     "0xeaC94633FFf8C65aD9EFdCF237741D931fa995Cd",
+		ContractType:        "SC",
+		WalletType:          "",
+		MetaTransactionType: "DEFAULT",
+		ABI:                 token.TestTokenABI,
+	}
 
-//resp, err := b.AddContract(data)
-//assert.Nil(t, err)
-//assert.NotNil(t, resp)
-//assert.Equal(t, resp.Code, 200)
-//}
+	resp, err := b.AddContract(data)
+	assert.Nil(t, err)
+	assert.NotNil(t, resp)
+	assert.Equal(t, resp.Code, 200)
+}
 
-//// Finished
-//func TestAddMethod(t *testing.T) {
-//b := buildBcnmy()
-//assert.NotNil(t, b)
+// Finished
+func TestAddMethod(t *testing.T) {
+	b := buildBcnmy()
+	assert.NotNil(t, b)
 
-//data := &metax.AddMethodRequest{
-//ContractAddress: "0xeaC94633FFf8C65aD9EFdCF237741D931fa995Cd",
-//ApiType:         "custom",
-//Name:            "mintTo",
-//MethodType:      "write",
-//Method:          "mintTo",
-//}
+	data := &metax.AddMethodRequest{
+		ContractAddress: "0xeaC94633FFf8C65aD9EFdCF237741D931fa995Cd",
+		ApiType:         "custom",
+		Name:            "mintTo",
+		MethodType:      "write",
+		Method:          "mintTo",
+	}
 
-//resp, err := b.AddMethod(data)
-//assert.Nil(t, err)
-//assert.NotNil(t, resp)
-//assert.Equal(t, resp.Code, 200)
-//}
+	resp, err := b.AddMethod(data)
+	assert.Nil(t, err)
+	assert.NotNil(t, resp)
+	assert.Equal(t, resp.Code, 200)
+}
 
-//// Finished
-//func TestCreateDapp(t *testing.T) {
-//b := buildBcnmy()
-//assert.NotNil(t, b)
+// Finished
+func TestCreateDapp(t *testing.T) {
+	b := buildBcnmy()
+	assert.NotNil(t, b)
 
-//data := &metax.CreateDappRequest{
-//DappName:             "test-create",
-//NetworkId:            "5",
-//EnableBiconomyWallet: false,
-//}
+	data := &metax.CreateDappRequest{
+		DappName:             "test-create",
+		NetworkId:            "5",
+		EnableBiconomyWallet: false,
+	}
 
-//resp, err := b.CreateDapp(data)
-//assert.Nil(t, err)
-//assert.NotNil(t, resp)
-//assert.Equal(t, resp.Code, 200)
-//}
+	resp, err := b.CreateDapp(data)
+	assert.Nil(t, err)
+	assert.NotNil(t, resp)
+	assert.Equal(t, resp.Code, 200)
+}
 
 // Finished
 func TestUniswapDemo(t *testing.T) {
